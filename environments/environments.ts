b6db6d1597c3bca9a15964839b08e29e142d3424
Vladimir <45611716+kirkHuman@users.noreply.github.com>
2024-11-21 21:11:20 -0800

    added prod urls to env

---
+++ environments/environments.ts
@@ -0,0 +1,13 @@
+export default {
+  qa: {},
+  preview: {},
+  preview2: {},
+  production: {
+    us: 'https://www.savagex.com',
+    eu: 'https://www.savagex.eu',
+    uk: 'https://www.savagex.co.uk',
+    fr: 'https://www.savagex.fr',
+    de: 'https://www.savagex.de',
+    es: 'https://www.savagex.es',
+  },
+};
